e624c9bbe7b40ad015457e3a6902147f7718d714
harbinger1210 <44458716+harbinger1210@users.noreply.github.com>
2019-06-16 11:15:39 +0800

    Update 1001(3n+1)猜想.cpp

--- 乙级/1001(3n+1)猜想.cpp
+++ 乙级/1001(3n+1)猜想.cpp
@@ -1,1 +1,21 @@
+#include<cstdio> 
 #include<iostream>
+using namespace std;
+int main()
+{
+	int n,i=0;
+	cin>>n;
+	while(n!=1)
+	{
+		if(n%2==0){
+			n=n/2;
+			i++;
+		}
+		else{
+			n=(n*3+1)/2;
+			i++;
+		}
+	}
+	cout<<i;
+	return 0;
+} 
